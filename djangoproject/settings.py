59363f71dfedb9c28bcfe4908a323d5c87630a7f
Soundar Balaji <70195209+SoundarBalaji@users.noreply.github.com>
2020-08-28 17:23:33 +0530

    Update settings.py

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*1g$zybuc0vs(x%n#ax@c7j3o%45!airw+f_1n1=g#4f^$!9)+'
+# SECURITY WARNING: keep the secret key used in production secret!  "*1g$zybuc0vs(x%n#ax@c7j3o%45!airw+f_1n1=g#4f^$!9)+"
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
